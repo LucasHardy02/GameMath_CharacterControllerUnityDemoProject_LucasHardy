fe12ab306e605d6a960452f8dae8b1c49c78b198
Lucas Hardy <lucashardywork@gmail.com>
2025-11-17 10:52:35 -0400

    Update

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -1,49 +1,97 @@
 using System;
 using Unity.Hierarchy;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class CharacterControllerScript : MonoBehaviour
 {
-    public float playerSpeed;
-    public int hop;
+    public float speed;
+    public float basespeed = 0f;
+    public float maxSpeed = 6f;
+    public float acceleration = 10f;
+    public float deceleration = 10f;
+    public float hop = 3;
+    public float gravity = -9.81f;
 
-    private Rigidbody rb;
+    private CharacterController controller;
     private bool isGrounded = false;
-    public GameObject player;
-    public GameObject Camera;
+    private Vector3 velocity;
+    
+    
     void Start()
     {
-        playerSpeed = 0.05f;
+        controller = GetComponent<CharacterController>();
+        speed = 0f;
+        maxSpeed = 6f;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         
-        if(Input.GetKey(KeyCode.W))
+
+        Vector3 direction = Vector3.zero;
+
+
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
         {
-            player.transform.Translate(Vector3.forward * playerSpeed);
+            velocity.y = -1f; 
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector3.forward;
+
         }
         if (Input.GetKey(KeyCode.A))
         {
-            player.transform.Translate(Vector3.left * playerSpeed);
+            direction += Vector3.left;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            player.transform.Translate(Vector3.right * playerSpeed);
+            direction += Vector3.right;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            player.transform.Translate(Vector3.back * playerSpeed);
+            direction += Vector3.back;
+
         }
-        if (isGrounded == true || Input.GetKey(KeyCode.Space))
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
-            player.transform.Translate(Vector3.up * hop);
+            velocity.y = hop;
+
+        }
+        if (isGrounded == false)
+        {
+            velocity.y += gravity * Time.deltaTime;
 
         }
 
+        direction = direction.normalized;
+
+        bool isMoving = direction.magnitude > 0.1f;
+
+        if (isMoving)
+        {
+            speed = Mathf.MoveTowards(speed, maxSpeed, acceleration * Time.deltaTime);
+
+        }
+        else
+        {
+            speed = Mathf.MoveTowards(speed, 0, deceleration * Time.deltaTime);
+
+        }
+
+
+        Vector3 movement = direction * speed * Time.deltaTime;
+
+        controller.Move(movement);
+        controller.Move(velocity * Time.deltaTime);
+
+       
+
+
     }
-    void PlayerMovement()
-    {
-        
-    }
+  
 }
